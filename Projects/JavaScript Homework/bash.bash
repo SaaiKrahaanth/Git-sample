2a6cfb7cfdc44d7dda6be8973e7c6dad8c34195d
Saai Krahaanth <126404799+SaaiKrahaanth@users.noreply.github.com>
2024-11-05 20:21:32 +0530

    learnings 5-11-2024 Brancing & Teamwork

---
+++ Projects/JavaScript Homework/bash.bash
@@ -0,0 +1,11 @@
+
+$ git clone maryrose-hw saai 
+Cloning into 'saai'... done. 
+$ cd saai 
+$ git branch saai-changes
+$ git add homework.js 
+$ git commit -m "commented" 
+[master 2daea85] commented 1 file changed, 1 insertion (+), 1 deletion (-)
+$ git push u origin saai-changes 
+Total 0 (delta 0), reused 0 (delta 0) To /home/ccuser/workspace/js-homework/maryrose-hw * [new branch] saai-changes -> saai-changes Branch saai-changes set up to track remote branch s aai-changes from origin. 
+$ cd ../maryrose-hw
---
+++ Projects/Ruby Time Calculator/bash.bash
@@ -0,0 +1,7 @@
+$ git add README.md 
+$ git add examples.md 
+$ git branch -d edits 
+error: The branch 'edits' is not fully merged. If you are sure you want to delete it, run 'git bra nch -D edits'. 
+$ git branch -D edits
+ Deleted branch edits (was 0dc9f6a). 
+$
